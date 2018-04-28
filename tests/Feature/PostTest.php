eb3009a1e667b3878089555ac35fbecc4cf6635a
Damilare <damilare.durojaye@gmail.com>
2018-04-28 06:41:26 +0100

    Test to see that a guest can see post title

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Post;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -10,10 +11,25 @@ class PostTest extends TestCase
 {
     use RefreshDatabase;
 
+    /**
+     * Test To see that a get can visit the blog page
+     */
     public function test_a_guest_can_access_blog_index()
     {
         $response = $this->get('/blog'); //make GET access to blog route
 
         $response->assertStatus(200); //assert http status return is 200
     }
+
+    /**
+     * Test to see that a guest can see posts
+     */
+    public function test_a_guest_can_see_post()
+    {
+        $post = factory(Post::class)->create();
+        $response = $this->get('/blog');
+        $response->assertSee($post->title);
+    }
+
+
 }
